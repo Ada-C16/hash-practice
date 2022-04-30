55bb0f3acd1abf96b82a0c7818bad4a1b1c15cdf
Mac <mackenziespeyton@gmail.com>
2022-04-29 19:14:53 -0700

    feat: add create_string_freq_map and grouped_anagrams

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,11 +1,53 @@
 
-def grouped_anagrams(strings):
-    """ This method will return an array of arrays.
-        Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+from collections import defaultdict
+
+def create_string_freq_map(strings):
     """
-    pass
+    Return a dictionary of dictionaries. 
+    Keys are of each string in strings and values of a dictionary 
+    that has keys of each letter in the string and values of how many 
+    times that letter appears in the string.
+    Time Complexity: O(n^2)
+    Space Complexity: O(n)
+    """
+    grouped_string_dicts = {}
+
+    for string in strings: 
+        string_dict = defaultdict(int)
+
+        for char in string:
+            string_dict[char] += 1
+
+        grouped_string_dicts[string] = string_dict
+
+    return grouped_string_dicts
+
+def grouped_anagrams(strings):
+    """ 
+    This method will return an array of arrays.
+    Each subarray will have strings which are anagrams of each other
+    Time Complexity: O(n^2)
+    Space Complexity: O(n)
+    """
+    grouped_anagrams = []
+
+    if not strings:
+        return grouped_anagrams
+
+    grouped_string_dicts = create_string_freq_map(strings)
+
+    for string in strings:
+        anagram_array = []
+        anagram_array.append(string)
+
+        for word, char_freq_dict in grouped_string_dicts.items():
+            if grouped_string_dicts[string] == char_freq_dict and word != string:
+                anagram_array.append(word)
+                strings.remove(word)
+
+        grouped_anagrams.append(anagram_array)
+
+    return grouped_anagrams
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements

e3050e5f771fa3d08f54414dd959a733f0e9f41f
Mariah Rosman <mrosman7@gmail.com>
2022-05-04 17:50:56 -0700

    completes anagram

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,11 +1,67 @@
 
+def to_dict(word):
+    anagram = {}
+
+    for letter in word:
+        if letter not in anagram:
+            anagram[letter] = 1
+        else:
+            anagram[letter] += 1
+
+    return anagram
+
+
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+
+    # dictionary where the key is a tuple with the letters and key is a list of the element
+    print("OG LIST: ", strings)
+    result_list = []
+    anagram_dict = {}
+
+    if len(strings) == 0:
+        return []
+
+    for word in strings:
+        letter_list = []
+        for letter in word:
+            letter_list.append(letter)
+            letter_list.sort()
+            ordered_word = "".join(letter_list)
+        hashed = hash(ordered_word)
+        if hashed in anagram_dict:
+            anagram_dict[hashed].append(word)
+        else:
+            anagram_dict[hashed] = [word]
+
+    for value in anagram_dict.values():
+        result_list.append(value)
+
+    return result_list
+    # print("WORD: ", word)
+    # if len(anagram_dict) < 1:
+    #     anagram_dict[word] = [word]
+    #     print(anagram_dict)
+    # else:
+    #     # dict_copy = anagram_dict.copy()
+    #     for key in anagram_dict.keys():
+    #         print("key: ", key)
+    #         print("value: ", anagram_dict[key])
+    #         if not is_anagram(word, key):
+    #             print("False")
+    #             anagram_dict[word] = [word]
+    #             print(anagram_dict)
+    #         else:
+    #             print("True")
+    #             anagram_dict[key].append(word)
+    #             print(anagram_dict)
+    #             print("----------------")
+
+    print("FINAL DICT: ", anagram_dict)
 
 
 def top_k_frequent_elements(nums, k):

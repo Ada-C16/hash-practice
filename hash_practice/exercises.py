83714ac6ef23d89b5e77af2a4c8afd756915f59d
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-05-05 14:35:14 -0700

    All tests for mandatory problems passing.:

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,20 +1,69 @@
-
-def grouped_anagrams(strings):
-    """ This method will return an array of arrays.
+""" This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+def grouped_anagrams(strings):
+    map = {}
+    for string in strings:
+        sort = str(''.join(sorted(string)))
+        if sort not in map: #whats the time complexity here
+            #what's the time complexity diff between this and .get()
+            map[sort] = [string]
+        else:
+            list = map[sort]
+            list.append(string)
+            map[sort] = list
+    
+    lists = []
+    for key in map:
+        lists.append(map[key])
+    return lists
+
+    #check out default dict for this problem too
+    #it's a way to only add things to the dict with certain conditions 
+
+    #there is a way to solve this without sort, like with something like counting 
+
+
+def makeMap(string):
+    map = {}
+
+    for char in string:
+        if not map.get(char):
+            map[char] = 1
+        else:
+            map[char] += 1
+    
+    return map
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
         Time Complexity: ?
         Space Complexity: ?
-    """
-    pass
+    """    
+    if not nums:
+        return []
+        
+    map = makeMap(nums)
 
+    pairs = []
+    for key in map:
+        pairs.append((map[key], key))
+
+    pairs.sort()
+
+    kItems = []
+    index = -1
+    count = 0
+    while count < k:
+        kItems.append(pairs[index][1])
+        index -= 1
+        count += 1
+
+    print(kItems)
+    return kItems 
 
 def valid_sudoku(table):
     """ This method will return the true if the table is still
@@ -25,5 +74,8 @@ def valid_sudoku(table):
         Time Complexity: ?
         Space Complexity: ?
     """
+    #
+
+
     pass
 

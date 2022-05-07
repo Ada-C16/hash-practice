199db590aac5a55119b10e47f0ac976b3b4c7b37
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-05-07 11:54:40 -0700

    Added big O and did half of optional problem.

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,14 +1,13 @@
 """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
     """
 def grouped_anagrams(strings):
     map = {}
     for string in strings:
         sort = str(''.join(sorted(string)))
-        if sort not in map: #whats the time complexity here
-            #what's the time complexity diff between this and .get()
+        if sort not in map: 
             map[sort] = [string]
         else:
             list = map[sort]
@@ -19,11 +18,6 @@ def grouped_anagrams(strings):
     for key in map:
         lists.append(map[key])
     return lists
-
-    #check out default dict for this problem too
-    #it's a way to only add things to the dict with certain conditions 
-
-    #there is a way to solve this without sort, like with something like counting 
 
 
 def makeMap(string):
@@ -40,12 +34,12 @@ def makeMap(string):
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
     """    
     if not nums:
         return []
-        
+
     map = makeMap(nums)
 
     pairs = []
@@ -74,8 +68,63 @@ def valid_sudoku(table):
         Time Complexity: ?
         Space Complexity: ?
     """
-    #
+    #need to manually check that every square, row, column is valid
+    #so make every square, row, column into a freq map
+    
+    for row in table:
+        map = {}
+        for block in row:
+            if block not in map:
+                map[block] = 1
+            elif block != ".": #if it's already in the map and NOT a ".", there's more than one 
+                return False
+    
+
+    columnNum = 0
+    while columnNum < 9:
+        rowNum = 0
+        map = {}
+        while rowNum < 9:
+            if table[rowNum][columnNum] not in map:
+                map[table[rowNum][columnNum]] = 1
+            elif block != ".":
+                return False
+            rowNum += 1
+        columnNum += 1
+    
+    #now i wanna slice for the squares
+    #square 1 is 
+    #table[0][0:3]
+    #table[1][0:3]
+    #table[2][0:3]
+
+    #square 2 is
+    #table[0][3:6]
+    #table[1][3:6]
+    #table[2][3:6]
+
+    #square 3 is
+    #table[0][6:9]
+    #table[1][6:9]
+    #table[2][6:9]
+
+    #square 4 is
+    #table[3][0:3]
+    #table[4][0:3]
+    #table[5][0:3]
+
+    squares = []
+    rowNum = 0
+    while rowNum < 7:
+        start = 0
+        stop = 3
+        square = []
+        while stop < 10:
+            square.append(table[rowNum][start:stop])
+
+    
 
 
-    pass
 
+    
+    

3ccdc6114c26207a39894484d844bd2f93c4ba90
brew doctor <>
2022-05-13 18:03:34 -0400

    completed mandatory parts

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,10 +2,21 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
+        Time Complexity: O(n)
         Space Complexity: ?
     """
-    pass
+    anagrams = {}
+    for word in strings:
+        sorted_word= ''.join(sorted(word))
+        if sorted_word not in anagrams:
+            anagrams[sorted_word] = [word]
+        else:
+            anagrams[sorted_word].append(word)
+    list_of_lists = []
+    for item in anagrams.values():
+        list_of_lists.append(item)
+    return list_of_lists
+
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
@@ -13,8 +24,22 @@ def top_k_frequent_elements(nums, k):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    "e.g.: Input: nums = [1,1,1,2,2,3], k = 2, Output: [1,2]"
+    
+    dict={}
+    list=[]
+    if nums == []:
+        return []
+    for num in nums:
+        if num in dict:
+            dict[num] += 1
+        else:
+            dict[num] = 1
+    other_list = sorted(dict.items(), key=lambda x:x[1], reverse=True) ##anonymous function
 
+    for i in range(k):
+        list.append(other_list[i][0])
+    return list
 
 def valid_sudoku(table):
     """ This method will return the true if the table is still

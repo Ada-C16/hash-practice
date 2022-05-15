c119a6ada9c3e486c73a43aec27b595b312b4fee
cecily-916 <cecily.marroquin@gmail.com>
2022-05-15 17:54:33 -0400

    completed top_k_frequent_elements

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,4 +1,7 @@
 
+from audioop import reverse
+
+
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
@@ -10,11 +13,40 @@ def grouped_anagrams(strings):
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
+        Time Complexity: 
         Space Complexity: ?
     """
-    pass
+    if len(nums) == 0:
+        return []
 
+    num_map = {}
+
+    for i in nums:
+        if i in num_map:
+            num_map[i] += 1
+        else:
+            num_map[i] = 1
+    
+    if len(num_map) == k:
+        return nums
+
+    reverse_num_map = {}
+
+    for key in num_map:
+        value = num_map[key]
+        if value in reverse_num_map:
+            reverse_num_map[value - .5] = key
+        else:
+            reverse_num_map[value] = key
+    
+    most_frequent = []
+    
+    for i in range(k):
+        max_freq_num = reverse_num_map[max(reverse_num_map)]
+        most_frequent.append(max_freq_num)
+        reverse_num_map.pop(max(reverse_num_map))
+    
+    return most_frequent
 
 def valid_sudoku(table):
     """ This method will return the true if the table is still

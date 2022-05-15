4890f129d1345ade2a047af5c0a3658063492527
syblades <86990570+syblades@users.noreply.github.com>
2022-05-15 16:12:55 -0700

    all top_k_frequent_elements tests are passing

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -4,9 +4,9 @@ def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
     """
-
+    
     anagrams = []
-    word_dict = get_anagrams(strings)
+    word_dict = anagrams_helper(strings)
     for word in word_dict.values():
         anagrams.append(word)
     return anagrams
@@ -18,23 +18,17 @@ def top_k_frequent_elements(nums, k):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    num_frequency = {}
+    for num in nums:
+        if not num in num_frequency.keys():
+            num_frequency[num] = 1
+        num_frequency[num] += 1	
+    k_most_frequent_keys = sorted(num_frequency, key=num_frequency.get, reverse=True)[:k]
+    return k_most_frequent_keys
 
 
-def valid_sudoku(table):
-    """ This method will return the true if the table is still
-        a valid sudoku table.
-        Each element can either be a ".", or a digit 1-9
-        The same digit cannot appear twice or more in the same 
-        row, column or 3x3 subgrid
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
-
-
-# helpers
-def get_anagrams(source_list):
+# anagram_helper
+def anagrams_helper(source_list):
     word_dict = defaultdict(list)
     for word in source_list:
         key = "".join(sorted(word))

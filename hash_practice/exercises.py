1c2f0be7e8b0a0972c9f8fadc6eccca8d88b1c2d
Lux Barker <lux.barker13@gmail.com>
2022-06-17 10:43:17 -0700

    pass kth frequent elements test

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict, deque
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
@@ -10,10 +10,27 @@ def grouped_anagrams(strings):
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    freq = defaultdict(int)
+    for num in nums: # O(n) where n is number of nums
+        freq[num] += 1
+
+    counts = defaultdict(deque)
+    max_count = 0
+    for num, count in freq.items(): # O(d) where d is number of distinct nums - at most equal to O(n) if all nums are distinct
+        counts[count].append(num)
+        if count > max_count:
+            max_count = count
+
+    results = []
+    while len(results) < k and max_count > 0: # O(c) where c is the highest frequency - at most equal to O(n) if all nums are distinct
+        if counts[max_count]:
+            results.append(counts[max_count].popleft())
+        else:
+            max_count -= 1
+    return results
 
 
 def valid_sudoku(table):

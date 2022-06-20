fce69b5256a6dbce5d214fd6bb7bb654727b1c56
Zandra Nguyen <zandra.eng@gmail.com>
2022-06-20 13:01:11 -0700

    getting ready for PR

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,18 +2,55 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(nk log k)
+        Space Complexity: O(n)
+
     """
-    pass
+    """ not sure why split() doesn't work:
+    # for word in strings:
+    #     split_word ="".join(word.split("").sort()) #aet
+    """
+    new_group_words = []
+    word_dict = {}
+
+    for word in strings:
+        split_word = []
+        for char in word:
+            split_word.append(char)
+            split_word.sort()
+        sorted_word = "".join(split_word)
+        if sorted_word not in word_dict:
+            word_list = [word]
+            word_dict[sorted_word] = word_list  # aet: [eat, tea]
+            new_group_words.append(word_list)
+        else:
+            word_dict[sorted_word].append(word)  # [eat, tea].append()
+
+    return new_group_words
+
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(nk)
+        Space Complexity: O(n)
     """
-    pass
+    num_dict = {}
+    for num in nums:
+        if num not in num_dict:
+            num_dict[num] = 1
+        else:
+            num_dict[num] += 1
+
+    sorted_values = list(num_dict.values())
+    sorted_values.sort(reverse=True)
+    new_array = sorted_values[:k]
+    new_list = []
+    for key, value in num_dict.items():
+        if value in new_array:
+            new_list.append(key)
+
+    return new_list
 
 
 def valid_sudoku(table):
@@ -25,5 +62,5 @@ def valid_sudoku(table):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
 
+    pass

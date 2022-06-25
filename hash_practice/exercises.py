67c9bf32aa4472bb673673e10917057716c69ae8
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2022-06-25 16:43:36 -0700

    solve top_k_frequent_elements problem

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,14 +2,13 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(nklog(k)), where k is the length of the string 
+        Space Complexity: O(log(n))
     """
     if not strings:
         return []
 
     result = {}
-   
     for word in strings:
         key = ''.join(sorted(word))
         if key in result:
@@ -28,7 +27,25 @@ def top_k_frequent_elements(nums, k):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not nums:
+        return []
+    count = {}
+    frequency = [[] for i in range(len(nums)+1)]
+    for n in nums:
+        if n not in count:
+            count[n] = 0
+        count[n] += 1
+
+    for key, val in count.items():
+        frequency[val].append(key)
+    
+
+    result = []
+    for i in range(len(frequency)-1, 0, -1):
+        for n in frequency[i]:
+            result.append(n)
+            if len(result) == k:
+                return result
 
 
 def valid_sudoku(table):

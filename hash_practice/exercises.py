4ed2c676243e42b12096c6a93c3abeb975ac729d
Veronica Osei <veronicaosei273@gmail.com>
2022-07-10 19:24:57 -0500

    finished without Sudoku

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,10 +2,20 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(N)
+        Space Complexity: O(N)
     """
-    pass
+    if len(strings) == 0:
+        return strings
+
+    anagram_dict = {}
+    for word in strings:
+        sorted_string = "".join(sorted(word))
+        if sorted_string not in anagram_dict:
+            anagram_dict[sorted_string] = [word]
+        else:
+            anagram_dict[sorted_string].append(word)
+    return list(anagram_dict.values())
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
@@ -13,8 +23,29 @@ def top_k_frequent_elements(nums, k):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
-
+    dict = {}
+    new_dict = {}
+    
+    for num in nums: 
+        if num in dict:
+            dict[num] += 1
+        if num not in dict:
+            dict[num] = 1
+    
+    
+    for ele,freq in dict.items():
+        if freq in new_dict:
+            new_dict[freq].append(ele)
+        else:
+            new_dict[freq] = [ele]
+    
+    order_freq = sorted(dict.values(),reverse=True)
+    new_list = []
+    for freq in order_freq:
+        new_list += new_dict[freq]
+    return new_list[:k]
+        
+    
 
 def valid_sudoku(table):
     """ This method will return the true if the table is still

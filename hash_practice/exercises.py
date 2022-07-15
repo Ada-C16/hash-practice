e6280ae6d7f8742477cc7b09c5c6a69a2cff0d3c
kaitlyngore <kaitlyngore@gmail.com>
2022-07-14 20:59:13 -0400

    add anagram and frequent_elements functions

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,18 +2,44 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: 
+        Space Complexity: O(n)
     """
-    pass
+    string_dict = {}
+    for string in strings:
+        key = ''.join(sorted(string))
+
+        if key in string_dict.keys():
+            string_dict[key].append(string)
+        else:
+            string_dict[key] = [string]
+        
+    return list(string_dict.values())
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) + O(n log n)??
+        Space Complexity: O(n)
     """
-    pass
+    frequency_map = {}
+    return_list = []
+
+    for num in nums:
+        if num in frequency_map:
+            frequency_map[num] += 1
+        else:
+            frequency_map[num] = 1
+
+    max_values = sorted(frequency_map.values(), reverse=True)
+        
+    for key, value in frequency_map.items():
+        if value in max_values[:k] and (len(return_list) < k):
+            return_list.append(key)
+
+    return return_list
+
+
 
 
 def valid_sudoku(table):

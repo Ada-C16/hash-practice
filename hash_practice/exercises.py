4db1eeef78fead0183581f859b958d0077bee89a
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2022-07-17 16:13:25 -0700

    solve validSudoku() function

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -24,8 +24,8 @@ def grouped_anagrams(strings):
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
     if not nums:
         return []
@@ -54,8 +54,25 @@ def valid_sudoku(table):
         Each element can either be a ".", or a digit 1-9
         The same digit cannot appear twice or more in the same 
         row, column or 3x3 subgrid
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+        Time Complexity: o(n^2)
+        Space Complexity: O(n)
+#     """
+from collections import defaultdict     
+def validSudoku(board):
+    if board == None:
+        return None
+    N = 9
+    cols = defaultdict(set)
+    rows = defaultdict(set)
+    squares = defaultdict(set)
 
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == ".":
+                continue
+            if(board[row][col] in rows[row] or board[row][col] in cols[col] or board[row][col] in squares[(row // 3, col // 3)]):
+                return False
+            cols[col].add(board[row][col])
+            rows[row].add(board[row][col])
+            squares[(row // 3, col // 3)].add(board[row][col])
+    return True

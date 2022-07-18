dd1e2c3257ec8aced35ef2ed14c892fda3f4bd6a
wangjiajia95 <86856561+wangjiajia95@users.noreply.github.com>
2022-07-18 15:16:50 -0700

    Update exercises.py

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -15,12 +15,7 @@ class MaxHeapObj(object):
         return f"{self.num}: {self.count}"
 
 def grouped_anagrams(strings):
-    """ This method will return an array of arrays.
-        Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    
+
     anagrams_dict = {}
 
     for word in strings:
@@ -40,11 +35,7 @@ def grouped_anagrams(strings):
 
 
 def top_k_frequent_elements(nums, k):
-    """ This method will return the k most common elements
-        In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
+
     number_count = {}
     for num in nums:
         if number_count.get(num):
@@ -96,14 +87,7 @@ def check_subgrid(table, subgrid):
     return True
 
 def valid_sudoku(table):
-    """ This method will return the true if the table is still
-        a valid sudoku table.
-        Each element can either be a ".", or a digit 1-9
-        The same digit cannot appear twice or more in the same 
-        row, column or 3x3 subgrid
-        Time Complexity: ?
-        Space Complexity: ?
-    """
+
     for i in range(0, len(table)):
         row_count = get_valid_digit_count()
         col_count = get_valid_digit_count()

91276fb84a65358b70bcd6dc71815b6e79f8d09a
Andrea Yuson <andreasyuson@gmail.com>
2022-07-21 10:17:43 -0700

    Hash Practice CSFUNn.

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,9 +1,9 @@
 def grouped_anagrams(strings):
-    # Method will return an array of arrays.
-    #     Each subarray will have strings which are anagrams of each other
-    #     TC: ?
-    #     SC: ?
-    # 
+    """ This method will return an array of arrays.
+        Each subarray will have strings which are anagrams of each other
+        Time Complexity: ?
+        Space Complexity: ?
+    """
     if not strings:
         return []
     res_list = [[strings[0]]]
@@ -33,11 +33,11 @@ def create_dict(word):
 
 
 def top_k_frequent_elements(nums, k):
-    #  Method will return the kth most common element
-    #     tie will select the first occuring element.
-    #     TC: O(nlogn)
-    #     SC: O(n)
-    # 
+    """ This method will return the k most common elements
+        In the case of a tie it will select the first occuring element.
+        Time Complexity: O(nlogn)
+        Space Complexity: O(n)
+    """
     
     nums_dict = create_dict(nums) 
     num_freq_tuple_list = []
@@ -58,14 +58,14 @@ def sort_by_tuple_freq(x) :
         
 
 def valid_sudoku(table):
-#    Method will return true if the table is 
-#         a valid sudoku table.
-#         Each element can either be a ".", or a digit 1-9
-#        Exact digit can't appear 2xs + in the same 
-#         row, column or 3x3 subgrid
-#         TC: O(1)
-#         SC O(1)
-# 
+    """ This method will return the true if the table is still
+        a valid sudoku table.
+        Each element can either be a ".", or a digit 1-9
+        The same digit cannot appear twice or more in the same 
+        row, column or 3x3 subgrid
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+"""
     for row in range(len(table)):
         if valid_row(row, table) == False:   
             return False
